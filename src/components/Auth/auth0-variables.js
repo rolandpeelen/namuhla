98b9ad29329c8445b14867150abaed1c7ccc2375
Roland Peelen <roland@peppr.nl>
2022-05-20 13:46:48 +0200

    :rocket: - Env. Variables

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,8 +1,23 @@
-export const AUTH_CONFIG = {
-  domain: "namuhla.eu.auth0.com",
-  clientId: "r2KFpw9TAx1bawPfa9YRHuFDHaZKSgWF",
-  callbackUrl: "http://localhost:3000/callback",
-  afterLogout: "http://localhost:3000",
-  audience: "http://localhost:3000",
-  scope: "openid profile email",
+const genConfig = () => {
+  const defaults = {
+    domain: "namuhla.eu.auth0.com",
+    clientId: "r2KFpw9TAx1bawPfa9YRHuFDHaZKSgWF",
+    callbackUrl: "http://localhost:3000/callback",
+    afterLogout: "http://localhost:3000",
+    audience: "http://localhost:3000",
+    scope: "openid profile email",
+  };
+
+  if (!process.env) return defaults;
+
+  return {
+    domain: process.env.REACT_APP_DOMAIN || defaults.domain,
+    clientId: process.env.REACT_APP_CLIENT_ID || defaults.clientId,
+    callbackUrl: process.env.REACT_APP_CALLBACK_URL || defaults.callbackUrl,
+    afterLogout: process.env.REACT_APP_AFTER_LOGOUT || defaults.afterLogout,
+    audience: process.env.REACT_APP_AUDIENCE || defaults.audience,
+    scope: process.env.REACT_APP_SCOPE || defaults.scope,
+  };
 };
+
+export const AUTH_CONFIG = genConfig();
